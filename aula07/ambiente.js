fead3afd737744148e3f9bf0100e8cef718e4b0d
VisonaN <paulovisona@gmail.com>
2023-10-12 16:18:21 -0300

    primeiro exercicio de repetição em Js

---
+++ aula07/ambiente.js
@@ -0,0 +1,23 @@
+/*
+var c = 1
+while (c <= 6) { // Estrutura de repetição com teste lógico no início
+    console.log(`Passo ${c}`)
+    c++ // Mesma coisa que c = c + 1
+}
+*/
+
+var c = 1
+do { // Estrutura de repetição com teste lógico no final
+    console.log(`Passo ${c}`)
+    c++ // Mesma coisa que c = c + 1
+} while (c <= 6)
+
+/*
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+console.log('Tudo bem?')
+*/
