064851a55666c7ecbfa8dadd2e9e7e57f1235018
VisonaN <paulovisona@gmail.com>
2023-10-12 16:46:43 -0300

    condições

--- aula07/ambiente.js
+++ aula07/ambiente.js
@@ -5,13 +5,13 @@ while (c <= 6) { // Estrutura de repetição com teste lógico no início
     c++ // Mesma coisa que c = c + 1
 }
 */
-
+/*
 var c = 1
 do { // Estrutura de repetição com teste lógico no final
     console.log(`Passo ${c}`)
     c++ // Mesma coisa que c = c + 1
 } while (c <= 6)
-
+*/
 /*
 console.log('Tudo bem?')
 console.log('Tudo bem?')
@@ -21,3 +21,9 @@ console.log('Tudo bem?')
 console.log('Tudo bem?')
 console.log('Tudo bem?')
 */
+
+// Estrutura de repetição com variável de controle
+
+for (var c = 1 /* Início */; c <= 10 /* Teste */; c++ /* Incremento */) {
+    console.log(`Passo ${c}`)
+}
