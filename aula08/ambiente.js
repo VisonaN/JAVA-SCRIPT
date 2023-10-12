6d84f8643eebd3869670e8d32e218addd9ba20ad
VisonaN <paulovisona@gmail.com>
2023-10-12 17:02:03 -0300

    Primeiro debug

--- aula08/ambiente.js
+++ aula08/ambiente.js
@@ -1,5 +1,6 @@
 // Estrutura de repetição com variável de controle
-
+console.log('Vai começar...')
 for (var c = 1 /* Início */; c <= 10 /* Teste */; c++ /* Incremento */) {
     console.log(c)
 }
+console.log('Fim')
