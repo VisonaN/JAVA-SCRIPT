337a72b63c20ea23b07d366bf0c6b0514ef5641f
VisonaN <paulovisona@gmail.com>
2023-10-12 10:44:04 -0300

    ex horas e imagens

--- aula06/modelo/script.js
+++ aula06/modelo/script.js
@@ -1,1 +1,21 @@
-alert('Olá!')
+
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        //BOM DIA!
+        document.body.style.background = '#dcd1ca'
+        img.src = 'imagens/foto_manha.png'
+    } else if (hora >= 12 && hora <= 18) {
+        //BOA TARDE!
+        img.src = 'imagens/foto_tarde.png'
+        document.body.style.background = '#9f6e5e'
+    } else {
+        //BOA NOITE!
+        img.src = 'imagens/foto_noite.png'
+        document.body.style.background = '#1e151b'
+    }
+}
