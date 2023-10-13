cec0d91563e535ef4f4d24dc2c698518a12d419a
VisonaN <paulovisona@gmail.com>
2023-10-13 10:34:35 -0300

    contador

--- aula09/ex016/script.js
+++ aula09/ex016/script.js
@@ -1,1 +1,30 @@
-alert('Olá!')
+function contar() {
+    let inicio = document.getElementById('txti')
+    let fim = document.getElementById('txtf')
+    let passo = document.getElementById('txtp')
+    let res = document.getElementById('res')
+
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        res.innerHTML = 'Impossível contar!'
+        window.alert('[ERRO] Faltam dados!')
+    } else {
+        res.innerHTML = 'Contando: <br>'
+        let i = Number(inicio.value)
+        let f = Number(fim.value)
+        let p = Number(passo.value)
+        if (p <= 0) {
+            window.alert('Passo Inválido! Considerar passo 1')
+            p = 1
+        }
+        if (i < f) { // Contagem crescente
+            for(let c = i; c <= f; c += p) {
+                res.innerHTML += ` ${c} \u{1F449}` // Assim que coloca emoji no JS "\u{}"
+            }
+        } else { // Contagem regressiva
+            for (let c = i; c >= f; c -= p) {
+                res.innerHTML += ` ${c} \u{1F449}`
+            }
+        }
+        res.innerHTML += `\u{1FAE1}` // O código do unicode (onde pega esses emoji) só funciona entre ``
+    }
+}
