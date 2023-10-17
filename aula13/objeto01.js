5b48dadb38dfdbdcff9c1b884389954e99456d04
VisonaN <paulovisona@gmail.com>
2023-10-17 10:13:33 -0300

    objetos em JS

---
+++ aula13/objeto01.js
@@ -0,0 +1,10 @@
+let amigo = {nome: 'Paulo', 
+sexo: 'M', 
+peso: 69,
+engordar(p) {
+    console.log('Engordou')
+    this.peso += p
+}}
+
+amigo.engordar(2)
+console.log(`${amigo.nome} pesa ${amigo.peso}Kg`)
